a1d2da7cd4e51fc1d6bfa01a35641052afe24715
hhyuw <high2092@gmail.com>
2023-04-27 15:06:27 +0900

    feat: 조언 DB 추가

---
+++ src/database/advice.ts
@@ -0,0 +1,96 @@
+export const OPTION_NAME_PLACEHOLDER = '?{option}';
+
+const ADVICES: Advice[] = [
+  {
+    name: `${OPTION_NAME_PLACEHOLDER} 효과를 25% 확률로 +1 올려`,
+    effect:
+      (beforeElixirs, { optionIdx }) =>
+      () => {
+        const result = [...beforeElixirs];
+        if (Math.random() * 100 <= 25) result[optionIdx].level++;
+        return result;
+      },
+    odds: 1,
+  },
+  {
+    name: `${OPTION_NAME_PLACEHOLDER} 효과를 50% 확률로 +1 올려`,
+    effect:
+      (beforeElixirs, { optionIdx }) =>
+      () => {
+        const result = [...beforeElixirs];
+        if (Math.random() * 100 <= 50) result[optionIdx].level++;
+        return result;
+      },
+    odds: 1,
+  },
+  {
+    name: `선택한 효과를 25% 확률로 +1 올려`,
+    effect: (beforeElixirs) => () => {
+      return (optionIdx) => {
+        const result = [...beforeElixirs];
+        if (Math.random() * 100 <= 25) result[optionIdx].level++;
+        return result;
+      };
+    },
+    odds: 1,
+  },
+  {
+    name: `선택한 효과를 50% 확률로 +1 올려`,
+    effect: (beforeElixirs) => () => {
+      return (optionIdx) => {
+        const result = [...beforeElixirs];
+        if (Math.random() * 100 <= 50) result[optionIdx].level++;
+        return result;
+      };
+    },
+    odds: 1,
+  },
+  {
+    name: `${OPTION_NAME_PLACEHOLDER} 효과를 -2 ~ +2 단계 올려`,
+    effect:
+      (beforeElixirs, { optionIdx }) =>
+      () => {
+        const result = [...beforeElixirs];
+        const diff = Math.floor(Math.random() * 5) - 2;
+        result[optionIdx].level += diff;
+        return result;
+      },
+    odds: 1,
+  },
+  {
+    name: `${OPTION_NAME_PLACEHOLDER} 효과를 -1 ~ +2 단계 올려`,
+    effect:
+      (beforeElixirs, { optionIdx }) =>
+      () => {
+        const result = [...beforeElixirs];
+        const diff = Math.floor(Math.random() * 4) - 1;
+        result[optionIdx].level += diff;
+        return result;
+      },
+    odds: 1,
+  },
+  {
+    name: `선택한 효과를 -2 ~ +2 단계 올려`,
+    effect: (beforeElixirs) => () => {
+      return (optionIdx) => {
+        const result = [...beforeElixirs];
+        const diff = Math.floor(Math.random() * 5) - 2;
+        result[optionIdx].level += diff;
+        return result;
+      };
+    },
+    odds: 1,
+  },
+  {
+    name: `선택한 효과를 -1 ~ +2 단계 올려`,
+    effect: (beforeElixirs) => () => {
+      return (optionIdx) => {
+        const result = [...beforeElixirs];
+        const diff = Math.floor(Math.random() * 4) - 1;
+        result[optionIdx].level += diff;
+        return result;
+      };
+    },
+    odds: 1,
+  },
+];
