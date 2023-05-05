b391d846aa46e07e9359a084139d7d2699cdbb5d
hhyuw <high2092@gmail.com>
2023-05-05 16:00:08 +0900

    design: favicon 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,10 +4,16 @@ import { Provider } from 'react-redux';
 import { store } from '../store';
 import { Footer } from '../components/Footer';
 import { ModalContainer } from '../components/ModalContainer';
+import Head from 'next/head';
 
 const App = ({ Component, pageProps }) => {
   return (
     <Provider store={store}>
+      <Head>
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
       <Component {...pageProps} />
       <Footer />
       <ModalContainer />
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,9 +4,7 @@ const Document = () => {
   return (
     <Html>
       <Head>
-        <meta name="mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="shortcut icon" href="image/favicon.svg" />
       </Head>
       <body>
         <Main />
