e2e9d7dd4d59d54cb1d5acefabec8575df6e6d8b
hhyuw <high2092@gmail.com>
2023-05-28 07:15:31 +0900

    chore: Google Analytics 코드 추가

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,25 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 const Document = () => {
   return (
     <Html>
       <Head>
+        <Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`} />
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
         <link rel="manifest" href="/manifest.json" />
         <link rel="shortcut icon" href="image/favicon.svg" />
         <meta name="theme-color" content="#a4907c" />
